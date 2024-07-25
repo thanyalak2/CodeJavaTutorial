9bc8afe507bb7523863a9b465953de80490ef105
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-07-25 14:29:12 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x, y;
+        x = 62;
+        y = 99;
+        float total;
+        System.out.println("x = " +x+ " y = " +y);
+        total = (float)x + y ;
+        System.out.println("total = " +total);
+     }
+}
