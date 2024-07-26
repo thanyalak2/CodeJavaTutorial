acd1b03ec9f69d600c7df69c75ead6464555d847
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-07-26 14:52:45 +0700

    Create calnum.java

---
+++ calnum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class calnum {
+    public static void main(String[] args) {
+       int x, y;
+       Scanner in  = new Scanner(System.in);
+       System.out.print("Input Number1 : ");
+       x = in.nextInt();
+       System.out.println("your number1 is "+x);
+       System.out.print("Input Number2 : ");
+       y = in.nextInt();
+       System.out.println("your number2 is "+y);
+       System.out.println(x+ " + " +y+ " = " +(x+y));
+    }
+}
