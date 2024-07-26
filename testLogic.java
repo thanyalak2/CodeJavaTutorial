0bc1baa6d7d7e80b4c38a296b372bf6afff4d3bf
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-07-26 14:52:28 +0700

    Create testLogic.java

---
+++ testLogic.java
@@ -0,0 +1,13 @@
+public class testLogic {
+    public static void main(String[] args) {
+        int x = 6, y = 9;
+        System .out.println(x > y);
+        System .out.println(x < y);
+        System .out.println(x >= y);
+        System .out.println(x == y);
+        System .out.println(x != 6);
+        System .out.println((x < y) && (y > 3));
+        System .out.println((x == 5) || (y > 2));
+        System .out.println(!(y > x));
+    }
+}
