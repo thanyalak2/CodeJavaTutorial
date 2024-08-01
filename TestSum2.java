e1b873059e6db5e48bc52b9e1643ee9cd8774909
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-01 10:04:08 +0700

    Create TestSum2.java

---
+++ TestSum2.java
@@ -0,0 +1,13 @@
+public class TestSum2 {
+        public static void main(String[] args) {
+        int i, sum = 0;
+        for(i = 1; i <= 100; i++){
+            if((i % 3)==0 || ((i % 5)==0)){
+                System.out.println(i+" ");
+                sum += i;
+            }
+        } 
+    System.out.println("total result : " +sum);
+    }
+    
+}
