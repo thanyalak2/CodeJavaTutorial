134561f51934850c3743d4aa3fda4077225d8670
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-01 13:44:58 +0700

    Create ExNo1.java

---
+++ ExNo1.java
@@ -0,0 +1,11 @@
+public class ExNo1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            // ตรวจสอบว่าตัวเลขเป็นเลขคู่
+            if (i % 2 == 0) {
+                // แสดงผลเลขคู่
+                System.out.println(i);
+            }
+        }
+    }
+}
