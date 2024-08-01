463f8f03c97126a9fa658ea683a9f18f0ce0c457
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-01 13:44:54 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,8 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i++) {
+            System.out.println("8 x " + i + " = " + (8 * i));
+        }
+    }
+}
+
