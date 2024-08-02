29f465c7d084fda68c29ceae26ddfa482b211949
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-02 10:02:12 +0700

    Create SimpleMethod.java

---
+++ SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main method.");
+        displayMassage();
+        System.out.println("Back in the main method.");
+    }
+    public static void displayMassage(){
+        System.out.println("Hello from the displayMessage method.");
+    }
+}
