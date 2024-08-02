c0291e24dc0d2b29dac18e7487691e721826f5ae
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-02 10:10:03 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            name();
+        }
+    }
+    public static void name(){
+        System.out.println("Thanyalak Buaphai");
+    }
+}
