e769731b1cde13b928f6326f10559d7f999f91f3
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-05 13:42:47 +0700

    Create ArrayInitia.java

---
+++ ArrayInitia.java
@@ -0,0 +1,8 @@
+public class ArrayInitia {
+    public static void main(String[] args) {
+        int [] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int index = 0; index < days.length; index++) {
+            System.out.println("Month " +(index + 1) + " has " +days[index]+" days.");
+        }
+    }
+}
