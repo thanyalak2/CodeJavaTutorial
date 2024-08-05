fdc133417f7e83b843b432959967be760764059f
thanyalak2 <152240977+thanyalak2@users.noreply.github.com>
2024-08-05 13:42:40 +0700

    Create PassElements.java

---
+++ PassElements.java
@@ -0,0 +1,11 @@
+public class PassElements {
+    public static void main(String[] args) {
+        int [ ] numbers = { 5, 10, 15, 20, 25, 30, 35, 40 };
+        for(int index = 0; index < numbers.length; index++) {
+            showValue(numbers[index]);
+        }
+    }
+    public static void showValue(int n) {
+        System.out.print(n+ " ");
+    }
+}
